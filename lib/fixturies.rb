e63cc3ddb55eada51f6bc042467a98e3b66516fa
Nate Brustein <nate@pedago.com>
2015-01-22 16:11:38 -0500

    some fixes

--- lib/fixturies.rb
+++ lib/fixturies.rb
@@ -2,6 +2,21 @@ class Fixturies
 
     class << self
 
+        attr_reader :fixtures_directory
+
+        def build(*things, &proc)
+            meth_name = :"builder_#{rand}"
+            define_method meth_name, &proc
+            things.each do |thing|
+                thing.is_a?(String) ? table(thing) : model(thing)
+            end
+            builders << meth_name
+        end
+
+        def set_fixtures_directory(dir)
+            @fixtures_directory = dir
+        end
+
         def create_fixtures
             self.new.create_fixtures
         end
@@ -12,14 +27,7 @@ class Fixturies
 
         def model(model_klass)
             self.table_names << model_klass.table_name
-        end     
-
-        def build(*things, &proc)
-            things.each do |thing|
-                thing.is_a?(String) ? table(thing) : model(thing)
-            end
-            builders << proc
-        end
+        end 
 
         def table_names
             unless defined? @table_names
@@ -39,7 +47,7 @@ class Fixturies
 
     attr_reader :record_identifiers
 
-    def intitialize
+    def initialize
         @record_identifiers = {}
     end
 
@@ -51,11 +59,14 @@ class Fixturies
 
     def build_all_records
         self.class.builders.each do |builder|
-            instance_eval(builder)
+            send(builder)
         end
     end
 
     def identify(record, name)
+        if record.id.nil?
+            raise ArgumentError.new("No id for record.  Must be saved before calling identify")
+        end
         record_identifiers[record_key(record)] = name
     end
 
@@ -66,6 +77,12 @@ class Fixturies
 
     private
     def create_fixture_files
+
+        if self.class.fixtures_directory.nil?
+            raise "No fixtures_directory set.  You must call set_fixtures_directory 'path/to/directory'"
+        end
+
+        FileUtils.mkdir_p(self.class.fixtures_directory)
 
         self.class.table_names.each do |table_name|
             
@@ -80,9 +97,8 @@ class Fixturies
                 name = record_identifiers[record_key(record)] || "#{table_name.singularize}_#{i}"
                 hash[name] = record.attributes
             end
-
-            FileUtils.mkdir_p(Rails.root.join('spec', 'fixtures'))
-            File.open(Rails.root.join('spec', 'fixtures', "#{table_name}.yml"), 'w+') do |f|
+            
+            File.open(Rails.root.join(self.class.fixtures_directory, "#{table_name}.yml"), 'w+') do |f|
                 f.write(hash.to_yaml)
             end
 
@@ -94,7 +110,7 @@ class Fixturies
         self.class.table_names.each do |table_name|
             quoted_table_name = ActiveRecord::Base.connection.quote_table_name(table_name)
             sql = "DELETE FROM #{quoted_table_name} "
-            ActiveRecord::Base.connection.delete(sql, "#{name} Delete all")
+            ActiveRecord::Base.connection.delete(sql, "#{quoted_table_name} Delete all")
         end
         
     end
